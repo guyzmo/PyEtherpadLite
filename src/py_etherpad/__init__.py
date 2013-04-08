d0f4b4b30ca874dcb0190d28944667d7f668c569
Bernard `Guyzmo` Pratz <guyzmo+github@m0g.net>
2013-04-08 12:57:48 +0200

    updated objects that are exposed through __init__

--- src/py_etherpad/__init__.py
+++ src/py_etherpad/__init__.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 """Module to talk to EtherpadLite API."""
 
+import Style
+import utils
+
 from Text import Text
 from Cursors import Cursors
 from Authors import Authors
 from Changeset import Changeset
 from Attributes import Attributes
 
-from EtherpadLiteClient import APIClient
+from APIClient import APIClient
 from SocketIOClient import EtherpadService
 
 from client import run as cli
